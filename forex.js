5fdbe119dfd185814c099372243ed711c48d0f82
Ji Ho Koo <jihokoo@gmail.com>
2014-08-21 23:36:34 -0400

    create exchange rate functions and tests

--- forex.js
+++ forex.js
@@ -1,3 +1,8 @@
+var fs = require('fs'),
+  request = require('request'),
+  Q = require('q'),
+  saved_rates;
+
 var Forex = function(filePath){
   if((this instanceof Forex) == false) {
       return new Forex(filePath);
@@ -10,10 +15,30 @@ var Forex = function(filePath){
   };
 };
 
-Forex.prototype.getLatestRate = function(){
-
+Forex.prototype.getLatestRate = function(currencyTo){
+  var deferred = Q.defer();
+  var self = this;
+  if(!currencyTo) deferred.reject({error: true, message: "please enter a currency code"});
+  request.get({url: 'http://openexchangerates.org/api/latest.json?app_id=0812d387198f4342bb179c7e1b9a2aa2', json:true}, function(err, r, body){
+    if(err) deferred.reject(err);
+    fs.writeFile(self.filePath(), JSON.stringify({conversionRates: {USD: body.rates}, date: Date.now()}, null, 4), function(err, datum){
+      if(err) deferred.reject(err);
+      var rate = body.rates[currencyTo];
+      var symbol = getSymbol(currencyTo);
+      deferred.resolve(symbol+ ' ' +rate);
+    });
+  });
+  return deferred.promise;
 };
 
-Forex.prototype.getSavedRate = function(){
-
+Forex.prototype.getSavedRate = function(currencyTo){
+  var deferred = Q.defer();
+  var self = this;
+  if(!currencyTo) deferred.reject({error: true, message: "please enter a currency code"});
+  var rate = require('./'+self.filePath()).conversionRates.USD[currencyTo];
+  var symbol = getSymbol(currencyTo);
+  deferred.resolve(symbol+ ' ' +rate);
+  return deferred.promise;
 };
+
+module.exports = Forex;
--- test/forex.test.js
+++ test/forex.test.js
@@ -1,10 +1,29 @@
 var Forex = require('../forex');
     request = require('request'),
-    should = require('should');
+    should = require('should'),
+    fs = require('fs'),
+    chai = require("chai"),
+    chaiAsPromised = require("chai-as-promised");
+
+var localeData;
+
+chai.use(chaiAsPromised);
+var assert = chai.assert;
+var expect = chai.expect;
 
 describe('forex', function() {
   var exchangeRate;
 
+  before(function(done) {
+    request.get({url: 'http://www.localeplanet.com/api/auto/currencymap.json', json:true}, function(e, r, body){
+      fs.writeFile('locale_data.json', JSON.stringify(body, null, 4), function(err, datum){
+        if(err) throw err;
+        localeData = require('../locale_data.json');
+        done();
+      });
+    });
+  });
+  
   beforeEach(function() {
     exchangeRate = new Forex('test_file.json');
   });
@@ -36,5 +55,55 @@ describe('forex', function() {
   it("should have methods 'getLatestRate' and 'getSavedRate'", function() {
     exchangeRate.getLatestRate.should.be.an.instanceOf(Function);
     exchangeRate.getSavedRate.should.be.an.instanceOf(Function);
+  });
+
+  it("updates the static file when getLatestRate is called", function(){
+    var oldTime = require('../'+exchangeRate.filePath()).date;
+    exchangeRate.getLatestRate('USD').then(function() {
+      var currentTime = require('../'+exchangeRate.filePath()).date;
+      currentTime.should.be.greaterThan(oldTime);
+      done();
+    }).catch(function(err){
+      done(err);
+    });
+  });
+
+  // it("can take a currency code and report the latest exchange rate", function() {
+  //  return expect(exchangeRate.getLatestRate('EUR')).to.eventually.equal('hello');
+  // });
+  // *** alternative method in mocha
+
+  it("can take a currency code and report a saved exchange rate", function(done) {
+    exchangeRate.getSavedRate('EUR').then(function(data) {
+      var rate = require('../'+exchangeRate.filePath()).conversionRates['USD']['EUR'];
+      var symbol = localeData['EUR']['symbol_native'];
+
+      data.should.equal(symbol+ ' ' +rate);
+      done();
+    }).catch(function(err){
+      done(err);
+    });
+  });
+  /*
+    If we were using just chai, we could do
+    return exchangeRate.getLatestRate('EUR').should.eventually.equal();
+    the only reason i am not is because the rest of the tests are written
+    with should
+
+    *** note for the future, chai > should
+  */
+
+  it("can be configured to return from static store or live api through a second parameter", function(done) {
+    var test = new Forex('test_file.json', true);
+    test.loadFromFile.should.be.true;
+
+    var oldTime = require('../'+exchangeRate.filePath()).date;
+    test.getRate('EUR').then(function(data) {
+      var currentTime = require('../'+exchangeRate.filePath()).date;
+      currentTime.should.equal(oldTime);
+      done();
+    }).catch(function(err){
+      done(err);
+    });
   });
 });
